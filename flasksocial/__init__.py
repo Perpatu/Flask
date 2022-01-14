b3433dd013af251aab182056f9d2a14c1444d546
Perpatu <84602799+Perpatu@users.noreply.github.com>
2022-01-14 06:22:11 +0100

    Update __init__.py

--- flasksocial/__init__.py
+++ flasksocial/__init__.py
@@ -7,31 +7,31 @@ import psycopg2.extras
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '53b2cdd9a055e2a6f6fd82e61067c6ea'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://njkjgxoxjmyvtk:81e33a32ec010d5743aedf2c7cedf8c312408eaeb9a96063c0c6667dec6f63a4@ec2-54-163-97-228.compute-1.amazonaws.com:5432/d6dsehs7hroh07'
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.init_app(app)
 login.login_view = 'login'
 login.login_message_category = 'info'
-DB_HOST = "ec2-54-163-97-228.compute-1.amazonaws.com"
-DB_NAME = "d6dsehs7hroh07"
-DB_USER = "njkjgxoxjmyvtk"
-DB_PASS = "81e33a32ec010d5743aedf2c7cedf8c312408eaeb9a96063c0c6667dec6f63a4"
+DB_HOST = ""
+DB_NAME = ""
+DB_USER = ""
+DB_PASS = ""
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 
 app.config['DEBUG'] = True
 app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = ''
 app.config['MAIL_PORT'] = '587'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 #app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'perpatussss@gmail.com'
-app.config['MAIL_PASSWORD'] = 'MG99100507139!!!!pl'
-app.config['MAIL_DEFAULT_SENDER'] = ('Perpatu from Society', 'perpatussss@gmail.com')
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_DEFAULT_SENDER'] = ''
 app.config['MAIL_MAX_EMAILS'] = None
 #app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['MAIL_ASCII_ATTACHMENTS'] = False
